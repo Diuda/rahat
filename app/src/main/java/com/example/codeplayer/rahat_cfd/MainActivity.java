a860ad03b912e425071853226788b2971e8750e1
Padmanabh Gupta <padmanabh14@gmail.com>
2018-10-20 05:02:51 +0530

    Added Chat activity and Message classes/UI

--- app/src/main/java/com/example/codeplayer/rahat_cfd/MainActivity.java
+++ app/src/main/java/com/example/codeplayer/rahat_cfd/MainActivity.java
@@ -46,9 +46,6 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     private ConnectionsClient mConnectionsClient;
-    private  final Map<String,Endpoint> mDiscoveredEndpoints  = new HashMap<>();
-    private final Map<String, Endpoint> mPendingConnections = new HashMap<>();
-    private final Map<String, Endpoint> mEstablishedConnections = new HashMap<>();
 
     BluetoothAdapter myDevice = BluetoothAdapter.getDefaultAdapter();
     String deviceName = myDevice.getName();
