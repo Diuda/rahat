b65cf3cd9cee8dc5cf30aa37defead81fa40f9a5
Diuda <divyanshu.1996@gmail.com>
2018-10-20 04:51:58 +0530

    created message repo

---
+++ app/src/main/java/com/example/codeplayer/rahat_cfd/MessageRepo.java
@@ -0,0 +1,29 @@
+package com.example.codeplayer.rahat_cfd;
+
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class MessageRepo {
+
+    private MessageDao messageDao;
+    private LiveData<List<messageStruct>> mAllMessages;
+
+    MessageRepo(Application application) {
+        AppDatabase db = AppDatabase.getDatabase(application);
+        messageDao = db.MessageDao();
+        mAllMessages = messageDao.getAllMessages();
+
+    }
+
+    LiveData<List<messageStruct>> getmAllMessages() {
+        return mAllMessages;
+    }
+
+    public void insert(messageStruct messageStruct) {
+//        new insertAsyncTask(messageDao).execute()
+    }
+}
