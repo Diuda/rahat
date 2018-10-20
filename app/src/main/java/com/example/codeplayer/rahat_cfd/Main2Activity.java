72e92a929854c91906165fa5843a0a537f27e1ff
Diuda <divyanshu.1996@gmail.com>
2018-10-21 04:00:23 +0530

    uid removed from data adding contructor

--- app/src/main/java/com/example/codeplayer/rahat_cfd/Main2Activity.java
+++ app/src/main/java/com/example/codeplayer/rahat_cfd/Main2Activity.java
@@ -67,7 +67,7 @@ public class Main2Activity extends AppCompatActivity {
 
         if(requestCode == 2 && resultCode == 1) {
             Log.i("messagekivalues", data.getStringExtra("MESSAGE"));
-            messageStruct messageStruct = new messageStruct(data.getIntExtra("UID", 0), data.getStringExtra("USERNAME"), data.getStringExtra("MESSAGE"));
+            messageStruct messageStruct = new messageStruct( data.getStringExtra("USERNAME"), data.getStringExtra("MESSAGE"));
             messageViewModel.insert(messageStruct);
 
         }
