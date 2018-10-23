f074a9ce095508642b520617af7d60d487abc3ed
Padmanabh Gupta <padmanabh14@gmail.com>
2018-10-23 22:11:53 +0530

    Modified for better coding practice

--- app/src/main/java/com/example/codeplayer/rahat_cfd/MessageListAdapter.java
+++ app/src/main/java/com/example/codeplayer/rahat_cfd/MessageListAdapter.java
@@ -2,6 +2,7 @@ package com.example.codeplayer.rahat_cfd;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,28 +13,55 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.MessageViewHolder> {
+public class MessageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    BluetoothAdapter myDevice;
-    String deviceName;
-    class MessageViewHolder extends RecyclerView.ViewHolder {
+    private BluetoothAdapter myDevice;
+    private String deviceName;
 
-        private final TextView messageItemView;
-        private final TextView nameItemView;
-        private final TextView timeItemView;
-
-        private  MessageViewHolder(View itemView) {
-            super(itemView);
-            messageItemView = itemView.findViewById(R.id.message_body);
-            nameItemView = itemView.findViewById(R.id.name);
-            timeItemView = itemView.findViewById(R.id.msgTime);
-        }
-
-    }
 
     private final LayoutInflater layoutInflater;
 
     private List<messageStruct> mMessages;
+
+    private class myMessageViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView messageItemView;
+
+
+        private  myMessageViewHolder(View itemView) {
+            super(itemView);
+            messageItemView = itemView.findViewById(R.id.message_body);
+
+        }
+
+    }
+
+    private class theirMessageViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView messageItemView;
+        private final TextView nameItemView;
+
+        private  theirMessageViewHolder(View itemView) {
+            super(itemView);
+            messageItemView = itemView.findViewById(R.id.message_body);
+            nameItemView = itemView.findViewById(R.id.name);
+        }
+
+    }
+
+    private class mapMessageViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView messageItemView;
+        private final TextView timeItemView;
+
+        private  mapMessageViewHolder(View itemView) {
+            super(itemView);
+            messageItemView = itemView.findViewById(R.id.message_body);
+
+            timeItemView = itemView.findViewById(R.id.msgTime);
+        }
+
+    }
 
     MessageListAdapter(Context context) {
         layoutInflater = LayoutInflater.from(context);
@@ -44,40 +72,51 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
     }
 
     @Override
-    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         if(viewType==0) {
             View itemView = layoutInflater.inflate(R.layout.my_message, parent, false);
-            return new MessageViewHolder(itemView);
+            return new myMessageViewHolder(itemView);
         }
         if(viewType==1){
             View itemView = layoutInflater.inflate(R.layout.their_message,parent,false);
-            return  new MessageViewHolder(itemView);
+            return  new theirMessageViewHolder(itemView);
         }
 
             View itemView = layoutInflater.inflate(R.layout.location_message,parent,false);
-            return new MessageViewHolder(itemView);
+            return new mapMessageViewHolder(itemView);
 
     }
-
 
     @Override
-    public void onBindViewHolder(MessageViewHolder holder, int position) {
-        if (mMessages != null) {
-            messageStruct current = mMessages.get(position);
-            holder.messageItemView.setText(current.getMessage());
-            if(holder.nameItemView!=null)
-            holder.nameItemView.setText(current.getUsername());
-            if(holder.timeItemView!=null){
-                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-                String str = sdf.format(new Date());
-                holder.timeItemView.setText(str);
-            }
-        } else {
-            // Covers the case of data not being ready yet.
-            holder.messageItemView.setText("No Word");
-        }
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+         int type= viewHolder.getItemViewType();
+         messageStruct message = mMessages.get(i);
+
+
+         if(type==0){
+             myMessageViewHolder mvh = (myMessageViewHolder)viewHolder;
+             mvh.messageItemView.setText(message.getMessage());
+         }
+         else if(type==1){
+             theirMessageViewHolder tmh = (theirMessageViewHolder)viewHolder;
+             tmh.messageItemView.setText(message.getMessage());
+             tmh.nameItemView.setText(message.getUsername());
+         }
+         else if(type==2){
+             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+             String str = sdf.format(new Date());
+
+
+             mapMessageViewHolder mmh = (mapMessageViewHolder)viewHolder;
+             mmh.messageItemView.setText(message.getMessage());
+             mmh.timeItemView.setText(str);
+         }
     }
+
+
+
 
     void setWords(List<messageStruct> words){
         mMessages = words;
