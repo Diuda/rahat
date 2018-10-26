52e91a5484fae7d0dc3c3c7f2e7fe786756a1665
Padmanabh Gupta <padmanabh14@gmail.com>
2018-10-26 07:04:27 +0530

    Specified layout for viewholder

--- app/src/main/java/com/example/codeplayer/rahat_cfd/AckParser.java
+++ app/src/main/java/com/example/codeplayer/rahat_cfd/AckParser.java
@@ -49,6 +49,7 @@ public class AckParser {
         Log.i("TIME t4",t4.toString());
 
 
-        return (((t4-t3)+(t3-t2))*299792.458f)/2;
+        return (((t4-t3)+(t2-t1))*299792.458f)/2.0f;
     }
+
 }
